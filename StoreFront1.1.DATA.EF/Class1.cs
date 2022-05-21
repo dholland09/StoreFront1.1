dcbd8f1b67610c4cb60e5470ad77da987254af0e
Derek Holland <dholland@live.com>
2022-05-21 13:39:37 -0500

    Add project files.

---
+++ StoreFront1.1.DATA.EF/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StoreFront1._1.DATA.EF
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ StoreFront1.1.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreFront1._1.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
